ca0e1ce3dcca052a6c5439d14f0c0789d09a9328
Paulo Uilian <uilianlbarros@gmail.com>
2021-06-22 18:17:52 -0300

    Arquivos da página

---
+++ js/events.js
@@ -0,0 +1,8 @@
+function ouvir() {
+    window.open('https://www.youtube.com/watch?v=TrtjJiLsV1E');
+}
+
+function assistir() {
+    window.open('https://www.youtube.com/watch?v=LJ4LMQ5UVUc&list=PLdkk-UyxUYSX4c9-rGvt4wwhxGGNns_k4&index=1');
+
+}
---
+++ js/owl/setup.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        400: {
+            items:1.5
+        },
+        500:{
+            items:2.5
+        },
+        730:{
+            items:3.5
+        },
+        950:{
+            items:4.5
+        },
+        1050:{
+            items:5.5
+        },
+        1200:{
+            items:6.5
+        }
+    }
+})
